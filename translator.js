344b059e619e84712d5f3744fe3ed8b6ec8d14e2
Michiel de Jong <michiel@unhosted.org>
2015-03-27 01:28:41 +0100

    support for server.pfx

--- translator.js
+++ translator.js
@@ -1,5 +1,7 @@
 var translator = require('./lib/translator'),
+    https = require('https'),
     http = require('http'),
+    fs = require('fs'),
     sslRootCAs = require('ssl-root-cas/latest'),
     checkArgs = require('./lib/args').checkArgs;
 
@@ -22,31 +24,38 @@ function determineFilename(urlPath, serverType) {
   }
 }
 
+function handler(req, res) {
+  console.log(req.headers);
+  if (checkCredentials(req.headers.authorization, argv['server-type-front'], argv['credentials-front'])) {
+    translator.send(argv['host-back'], argv['port-back'], argv['base-path-back'], argv['credentials-back'],
+        determineFilename(req.url, argv['server-type-front']), req.headers['content-type'], req,
+        argv['server-type-back'], false, function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        res.end(err.toString());
+      } else {
+        res.writeHead(data.statusCode, data.responseHeaders);
+        res.end(data.responseBody);
+      }
+    });
+  } else {
+    res.writeHead(401);
+    res.end('please match the translator\'s --credentials-front argument in your Authorization header');
+  }
+}
+
 function run() {
   var argv = checkArgs(['server-type-front', 'server-type-back', 'host-back', 'port-back', 'base-path-back', 'port-front',
                  'credentials-back', 'credentials-front']);
   sslRootCAs.inject().addFile('./ca.pem');
   if (argv) {
-    http.createServer(function(req, res) {
-console.log(req.headers);
-      if (checkCredentials(req.headers.authorization, argv['server-type-front'], argv['credentials-front'])) {
-        translator.send(argv['host-back'], argv['port-back'], argv['base-path-back'], argv['credentials-back'],
-            determineFilename(req.url, argv['server-type-front']), req.headers['content-type'], req,
-            argv['server-type-back'], false, function (err, data) {
-          if (err) {
-            res.writeHead(500);
-            res.end(err.toString());
-          } else {
-            res.writeHead(data.statusCode, data.responseHeaders);
-            res.end(data.responseBody);
-          }
-        });
-      } else {
-        res.writeHead(401);
-        res.end('please match the translator\'s --credentials-front argument in your Authorization header');
-      }
-    }).listen(argv['port-front']);
-    console.log('There is now a ' + argv['server-type-front'] + ' to ' + argv['server-type-back'] + ' translator on http://localhost:' + argv['port-front']);
+    if (fs.existsSync('./server.pfx')) {
+      https.createServer({ pfs: fs.readFileSync('./server.pfx') }, handler).listen(argv['port-front']);
+      console.log('There is now a ' + argv['server-type-front'] + ' to ' + argv['server-type-back'] + ' translator on https://<domain.com>:' + argv['port-front']);
+    } else {
+      http.createServer(handler).listen(argv['port-front']);
+      console.log('There is now a ' + argv['server-type-front'] + ' to ' + argv['server-type-back'] + ' translator on http://localhost:' + argv['port-front']);
+    }
   }
 }
 
